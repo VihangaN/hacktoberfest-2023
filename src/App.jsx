4c1d85e716f1341341cc3319f3e0dc235578a73d
Vihanga nivarthana <it18091380@my.sliit.lk>
2023-09-26 23:54:37 +0530

    Initial project setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import './App.scss';
 
 function App() {
-  return <div className="main-container"></div>;
+  return (
+    <div className="main-container">
+      <nav className="top-nav-bar">v</nav>
+      <div className="map-view-container"></div>
+      <div className="footer-bar"></div>
+    </div>
+  );
 }
 
 export default App;
