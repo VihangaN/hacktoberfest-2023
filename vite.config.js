e244fc96570a8f9b30115eb322e17e5707b55e7b
Vihanga nivarthana <it18091380@my.sliit.lk>
2023-09-26 23:41:06 +0530

    Initial project setup

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import { fileURLToPath } from 'url';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "@/styles/_theme.scss";`,
+      },
+    },
+  },
+});
