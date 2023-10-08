d52614192c564a863bbfc91e3494298aa8450dcc
Vihanga nivarthana <it18091380@my.sliit.lk>
2023-10-09 02:42:55 +0530

    refactor:rename country select component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useRef } from 'react';
 import './App.scss';
 import MapView from './components/Map';
-import NavBar from './components/Navbar';
+import NavBar from './components/CountrySelect';
 import 'bootstrap/dist/css/bootstrap.css';
 
 function App() {
---
+++ src/components/CountrySelect.jsx
@@ -0,0 +1,77 @@
+import React, { useState } from 'react';
+import { countries } from 'countries-list';
+
+const CountrySelect = () => {
+  const countryArr = Object.entries(countries).map(([code, value]) => ({
+    code,
+    value,
+  }));
+
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const filteredCountries = countryArr.filter((country) =>
+    country.value.name.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container-fluid">
+        <div className="navbar-brand" href="#">
+          Where are you from?
+        </div>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNavDarkDropdown"
+          aria-controls="navbarNavDarkDropdown"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNavDarkDropdown">
+          <ul className="navbar-nav">
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link dropdown-toggle text-warning"
+                href="#"
+                id="navbarDarkDropdownMenuLink"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Country
+              </a>
+              <ul
+                className="dropdown-menu dropdown-menu-dark"
+                aria-labelledby="navbarDarkDropdownMenuLink"
+              >
+                <li className="country-select">
+                  <input
+                    type="text"
+                    className="search-country-input"
+                    placeholder="Search country"
+                    value={searchQuery}
+                    onChange={(e) => setSearchQuery(e.target.value)}
+                  />
+                </li>
+                {filteredCountries?.map((country) => {
+                  return (
+                    <li key={country.code}>
+                      <span className="dropdown-item text-warning">
+                        {country?.value.name}
+                      </span>
+                    </li>
+                  );
+                })}
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default CountrySelect;
