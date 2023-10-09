c743746b2edd24724b9c62fd8db813a620e26492
Dilshan Ramesh <dilshanramesh81@gmail.com>
2023-10-10 00:22:16 +0530

    get location data by using Weather API

--- src/components/CountrySelect.jsx
+++ src/components/CountrySelect.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from 'react';
+import { useState, useContext } from 'react';
 import { countries } from 'countries-list';
 import { Appcontext } from '../context/appContext';
 
---
+++ src/utils/constants.js
@@ -0,0 +1,8 @@
+
+const constants = {
+    WEATHER_API_BASE_URL: `${import.meta.env.VITE_WEATHER_API_URL}`,
+    WEATHER_API_KEY: `${import.meta.env.VITE_WEATHER_API_KEY}`,
+
+};
+
+export default constants;
